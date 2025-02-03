716515df8da1c1d130ab3f53c042741acfa4639a
EmilyLangenfeld <emily.langenfeld@mail.utoronto.ca>
2025-02-03 11:22:25 -0500

    minor changes

--- CalculateCovarianceMatrix.py
+++ CalculateCovarianceMatrix.py
@@ -8,8 +8,7 @@ def calculate_cov_matrix(d, fwr):
     filtered_bonds = []
     for df in d:
         bonds = d[df]
-        # filtered_bonds.append(bonds[bonds['MATURITY DATE'].dt.month != 9])
-        filtered_bonds.append(bonds.iloc[0:5])
+        filtered_bonds.append(bonds[bonds['MATURITY DATE'].dt.month != 9])
 
     for i in filtered_bonds:
         ytms = []
@@ -20,14 +19,14 @@ def calculate_cov_matrix(d, fwr):
     for ytms in list_of_ytm:
         log_return = []
         for i in range(len(ytms) - 1):
-            log_return.append(np.log((ytms[i+1]-ytms[i])/ytms[i]))
+            log_return.append(np.log((ytms[i+1])/ytms[i]))
         log_returns_y.append(log_return)
 
     log_returns_f = []
     for forwards in fwr:
         log_return = []
         for i in range(len(forwards)-1):
-            log_return.append(np.log((forwards[i+1]-forwards[i])/forwards[i]))
+            log_return.append(np.log((forwards[i+1])/forwards[i]))
         log_returns_f.append(log_return)
 
     log_returns_arr = np.array(log_returns_y).T
